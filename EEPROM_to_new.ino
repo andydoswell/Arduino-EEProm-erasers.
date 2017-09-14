cf223e2192e4f2336f2dfc3434c4d1f51d983b95
Andy Doswell <andydoswell@hotmail.com>
2017-09-14 15:14:33 +0100

    Add files via upload

---
+++ EEPROM_to_new.ino
@@ -0,0 +1,43 @@
+ #include <EEPROM.h> //EEPROM interface
+ int addr = 0;
+ float len;
+ int pcComplete;
+
+void setup() {
+
+ Serial.begin (9600);
+ Serial.println ("EEPROM Eraser");
+ Serial.print("Size of EEPROM =");
+ len = EEPROM.length();
+ Serial.print (len);
+ Serial.println (" bytes");
+ delay (1000);
+ pinMode(13, OUTPUT);
+ }
+
+
+
+void loop() {
+ EEPROM.write (addr,0xFF);
+ Serial.print (addr);
+ Serial.print ("  ");
+ pcComplete = (addr / len)*100;
+ Serial.print (pcComplete);
+ Serial.println ("%");
+ addr ++;
+ if (addr == len+1){
+  addr = 0;
+  Serial.println ("Complete");
+  blinkPin();
+  
+ }
+ }
+
+ void blinkPin () {
+  while (1) {
+  digitalWrite(13, HIGH);  
+  delay(1000);             
+  digitalWrite(13, LOW);   
+  delay(1000);             
+  }
+ }
